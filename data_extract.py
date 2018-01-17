a2d4bdcb637113061cf9fc592c256b00020fec2d
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-01-17 07:30:41 +0000

    Update data_extract.py

--- data_extract.py
+++ data_extract.py
@@ -15,6 +15,7 @@ req.raise_for_status()
 soup = bs4.BeautifulSoup(req.text)
 
 for link in soup.find_all('h3'):
-	if link.text == 'Browse Wine':
-		wine = link.find_next_sibling('ul')
-		for i in wine:
+    if link.text == 'Browse Wines':
+        for tag in link.findNext('ul'):
+            tag = str(tag)
+            wines.append(tag)
