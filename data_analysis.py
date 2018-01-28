265e67b6306e796026289250ca2a87bd4c6c341e
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-01-28 16:05:10 +0000

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,7 @@
 from data_extract import *
 import seaborn as sns
 import matplotlib.pyplot as plt
-from stats_functions import draw_perm_reps
+from scipy import stats
 
 #EDA on the wine's country of origin and rating score
 _ = sns.swarmplot(x='country', y='rating_score', data=df_country)
@@ -30,8 +30,43 @@ samples = np.random.normal(mu, sigma, size=10000)
 plt.hist(samples, normed=True, bins=100, histtype='step')
 #Yes - data is normally ditributed
 
-#
+#what's the probability of someone buying non french wine again
 country_mean = pd.DataFrame(df_country.groupby('country')['rating_score'].mean())
 french_mean = country_mean.filter(like='french', axis=0)['rating_score'].values
 prob = np.sum(samples >= french_mean) / len(samples)
 print('Probability of being better than french wine: ', prob)
+
+#Hypothesis test does the origin of wine make a difference to whether someone will buy the wine again
+#Null Hypothesis: Country of origin has no impact on whether someone will buy wine again
+#Alternative Hypothesis: Country of origin has an impact on whether someone will buy wine again
+
+#sample mean
+world_mean = pd.DataFrame(df_country.groupby('world')['rating_score'].mean())
+new_mean = world_mean.filter(like='new world', axis=0)['rating_score'].values
+old_mean = world_mean.filter(like='old world', axis=0)['rating_score'].values
+
+#sample variance
+world_var = pd.DataFrame(df_country.groupby('world')['rating_score'].std())
+new_var = world_var.filter(like='new world', axis=0)['rating_score'].values
+old_var = world_var.filter(like='old world', axis=0)['rating_score'].values
+
+#sample size
+world_cnt = pd.DataFrame(df_country.groupby('world')['rating_score'].count())
+new_cnt = world_cnt.filter(like='new world', axis=0)['rating_score'].values
+old_cnt = world_cnt.filter(like='old world', axis=0)['rating_score'].values
+
+#samples
+rvs_new = stats.norm.rvs(loc=new_mean, scale=new_var, size=new_cnt)
+rvs_old = stats.norm.rvs(loc=old_mean, scale=old_var, size=old_cnt)
+
+#t-test and p value
+t_test, p_value = stats.ttest_ind(rvs_new, rvs_old, equal_var = False)
+sig_level = 0.05
+
+if p_value > sig_level:
+  print('p value: ', p_value, ' is greater than the significance level of ', sig_level)
+  print('Cannot reject null hypothesis, country of origin has no impact on sales')
+else:
+  print('p value: ', p_value, ' is less than the significance level of ', sig_level)
+  print('Reject null hypothesis, country of origin has an impact on sales')
+
