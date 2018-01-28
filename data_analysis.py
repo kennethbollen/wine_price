7639243c849729daed179d11574ce9e4c9c5f11c
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-01-28 14:59:48 +0000

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,6 @@
 from data_extract import *
 import seaborn as sns
+import matplotlib.pyplot as plt
 from stats_functions import draw_perm_reps
 
 #EDA on the wine's country of origin and rating score
@@ -22,3 +23,15 @@ _ = sns.swarmplot(x='world', y='rating_score', data = df_country)
 #numerical analysis on the wine's world of origin and rating score
 print(df_country.groupby('world')[['price','rating_score']].mean())
 
+#is the data normally distributed
+mu = df_country['rating_score'].mean()
+sigma = df_country['rating_score'].std()
+samples = np.random.normal(mu, sigma, size=10000)
+plt.hist(samples, normed=True, bins=100, histtype='step')
+#Yes - data is normally ditributed
+
+#
+country_mean = pd.DataFrame(df_country.groupby('country')['rating_score'].mean())
+french_mean = country_mean.filter(like='french', axis=0)['rating_score'].values
+prob = np.sum(samples >= french_mean) / len(samples)
+print('Probability of being better than french wine: ', prob)
