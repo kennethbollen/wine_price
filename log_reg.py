62e3158b15f98b837d0675b9f2b1923819ef5067
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-12 20:54:27 +0000

    Update and rename data_pipeline.py to log_reg.py

---
+++ log_reg.py
@@ -0,0 +1,70 @@
+from data_extract import *
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
+
+# Create the classifier: logreg
+logreg = LogisticRegression()
+# Fit the classifier to the training data
+logreg.fit(X_train, y_train)
+
+# Predict the labels of the test set: y_pred
+y_pred = logreg.predict(X_test)
+
+# Compute and print the confusion matrix and classification report
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+# Compute predicted probabilities: y_pred_prob
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
+
+# Generate ROC curve values: fpr, tpr, thresholds
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+
+# Plot ROC curve
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+
+# Compute predicted probabilities: y_pred_prob
+# Returns the probability of a given sample being in a particular class
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
+
+# Compute and print AUC score
+print("AUC: {}".format(roc_auc_score(y_test, y_pred_prob)))
+
+# Compute cross-validated AUC scores: cv_auc
+cv_auc = cross_val_score(logreg,X,y,cv=5,scoring='roc_auc')
+
+# Print list of AUC scores
+# if the model is greater than 0.5 than it shows signs that it's a strong model (better than guessing)
+print("AUC scores computed using 5-fold cross-validation: {}".format(cv_auc))
+
+# Setup the hyperparameter grid
+# C controls the inverse of the regularization strength
+# A large C can lead to an overfit model, while a small C can lead to an underfit model
+c_space = np.logspace(-5, 8, 15)
+param_grid = {'C': c_space}
+
+# Instantiate the GridSearchCV object: logreg_cv
+print('finding optimal regularization strength, in this hyperparameter space...')
+logreg_cv = GridSearchCV(logreg, param_grid, cv=5)
+
+# Fit it to the data
+logreg_cv.fit(X, y)
+
+# Print the tuned parameters and score
+print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_)) 
+print("Best score is {}".format(logreg_cv.best_score_))
+
+#NEXT STEP: HOLD OUT https://campus.datacamp.com/courses/supervised-learning-with-scikit-learn/fine-tuning-your-model?ex=14
